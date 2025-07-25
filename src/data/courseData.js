f5cd0e0fe547e44a4a652f76e1f1590b50d5ea78
nhut0902-pr <lamminhnhut09022011@gmail.com>
2025-07-25 20:53:20 +0700

    Create courseData.js

---
+++ src/data/courseData.js
@@ -0,0 +1,29 @@
+export const courseData = [
+  {
+    id: "intro",
+    title: "Python Introduction",
+    lessons: [
+      { id: "getting-started", title: "Getting Started with Python" },
+      { id: "variables", title: "Python Variables and Data Types" },
+      { id: "operators", title: "Python Operators" }
+    ]
+  },
+  {
+    id: "flow-control",
+    title: "Python Flow Control",
+    lessons: [
+      { id: "if-else", title: "If...else Statement" },
+      { id: "loops", title: "For and While Loops" },
+      { id: "break-continue", title: "Break and Continue" }
+    ]
+  },
+  {
+    id: "functions",
+    title: "Python Functions",
+    lessons: [
+      { id: "defining-functions", title: "Defining Functions" },
+      { id: "arguments", title: "Function Arguments" },
+      { id: "recursion", title: "Recursion" }
+    ]
+  }
+];
