fbfac2f71dcf472a0dae5538885f89a538e063d8
nhut0902-pr <lamminhnhut09022011@gmail.com>
2025-07-25 20:54:20 +0700

    Create exampleData.js

---
+++ src/data/exampleData.js
@@ -0,0 +1,10 @@
+export const exampleData = [
+  "Hello World",
+  "Add Two Numbers",
+  "Calculate the Area of a Triangle",
+  "Swap Two Variables",
+  "Generate a Random Number",
+  "Check if a Number is Prime",
+  "Find the Factorial of a Number",
+  "Fibonacci Sequence"
+];
